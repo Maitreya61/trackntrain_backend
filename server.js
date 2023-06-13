91d85ffefde871b411d1c08a9be46d5973968e50
Maitreya61 <maitreyaoffl@gmail.com>
2023-06-14 00:49:45 +0530

    third commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workout');
+const PORT = process.env.PORT || 4000
 
 //Express app
 const app = express();
@@ -14,7 +15,7 @@ app.use('/api/workouts', workoutRoutes);
 mongoose.connect(process.env.DB_URI)
     .then(()=>{
         app.listen(process.env.PORT, ()=>{
-            console.log(`Connected to DB and listening on Port`, process.env.PORT)
+            console.log(`Connected to DB and listening on Port`, PORT)
         }) 
     })
     .catch((error)=>{
