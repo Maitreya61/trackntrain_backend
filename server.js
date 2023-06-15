244429dd74d47e0de5f71d4154d47ced826bd853
Maitreya61 <maitreyaoffl@gmail.com>
2023-06-15 12:53:33 +0530

    12 commit

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    res.status(200).json("Hello")
+    next()
+  })
+
 app.use(cors({
     origin:"https://trackntrain.onrender.com/api",
     methods: ["GET","POST","DELETE","PATCH"]
